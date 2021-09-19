d85ce3a40df04dfbc45608abdcbf2e4f1593a7a4
Thiloretti <thiagoloretti@outlook.com>
2021-09-19 16:41:19 -0300

    Department Services

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,44 +15,54 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentServices;
 
 public class DepartmentListController implements Initializable {
+
+	private DepartmentServices service;
+	
+	public void setDepartmentServices(DepartmentServices service) {
+		this.service = service;
+	}
+	
+	private ObservableList<Department> obsList;
+	
+	public void updateTableView() {
+		if(service == null) {
+			throw new IllegalStateException("State was null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
+	}
 	
 	@FXML
 	private TableView<Department> tableViewDepartment;
-	
+
 	@FXML
 	private TableColumn<Department, Integer> tableColumnId;
-	
+
 	@FXML
 	private TableColumn<Department, String> tableColumnName;
-	
+
 	@FXML
 	private Button btNew;
-	
+
 	@FXML
-	public void onBtNewAction(){
+	public void onBtNewAction() {
 		System.out.println("onBtNewAction");
 	}
-	
-	
-	
-	
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		initializeNodes();
-		
+
 	}
-
-
-
-
 
 	private void initializeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
-		
+
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 	}
